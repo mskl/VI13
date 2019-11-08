be45318bddf5cd43060d8ec99966caee36f8ad23
Matyáš Skalický <matyaskalicky@gmail.com>
2019-11-08 16:49:32 +0000

    Map of europe now works

--- src/js/map.js
+++ src/js/map.js
@@ -3,23 +3,25 @@ var svg = d3.select("svg"),
     height = +svg.attr("height");
 
 var path = d3.geoPath();
-var ms;
+var world;
 
 var promises = [
-    // The map itself
-    d3.json("data/map/us-10m.v1.json"),
-    // d3.json("data/map/world-110m.v1.json"),
+    d3.json("data/map/countries-10m.json")
 ];
+
+var projection = d3.geoMercator().center([10,52]).scale(600);
+
+path = path.projection(projection);
 
 // Load all of the promises and then call the ready funtion
 Promise.all(promises).then(ready);
 
-function ready([mapshapes], reject) {
-    ms = mapshapes;
+function ready([wr], reject) {
+    world = wr;
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
-        .data(topojson.feature(mapshapes, mapshapes.objects.counties).features)
+        .data(topojson.feature(world, world.objects.countries).features)
         .enter().append("path")
         .attr("fill", "rgb(155,23,42)")
         .attr("d", path);
