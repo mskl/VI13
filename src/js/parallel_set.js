786d8d4864bdf71b58f0e9aa1b3fa6d5382664d4
Ingeborg Sollid <ingsollid@gmail.com>
2019-11-24 12:04:12 +0000

    Add sending selected country, and changes on hover node

--- src/js/parallel_set.js
+++ src/js/parallel_set.js
@@ -3,6 +3,8 @@ var previousNodeColor;
 var linksToSelectedNode;
 var selectedLink;
 
+var selected_country = null;
+
 
 var svg = d3.select("#parallel_set > svg"),
     setsWidth = +svg.style("width").replace("px", ""),
@@ -19,8 +21,10 @@ let formatNumber = d3.format(",.0f"),
     format = function(d) { return formatNumber(d) + " students"; },
     parallelsets_color = d3.scaleOrdinal(d3.schemeCategory10);
 
-d3.json("data/parallel_sets/degree_flow.json").then(function (data) {
-    degree_data = data;
+
+d3.json("data/parallel_sets/receiving_countryselection_degree.json").then(function (data) {
+    degree_data = data['NO'];
+
     gen_vis();
 });
 
@@ -70,6 +74,7 @@ function gen_vis() {
         .attr("height", function(d) { return d.y1 - d.y0 + 3; })
         .attr("width", function(d) { return d.x1 - d.x0; })
         .attr("fill", function(d) { return parallelsets_color(d.name.replace(/ .*/, "")); })
+        .attr("fill-opacity", 0.7)
         .attr("title", function (d) {return d.name + d.index})
         .attr("stroke", "#000")
         .on("mouseover", function(d){
@@ -113,7 +118,7 @@ dispatch.on("mouseoverNode", function(node){
     }
     selectedNode = d3.select("rect[title=\'" + node.name + node.index + "\']");
     //previousColor = selectedNode.style.background;
-    selectedNode.attr("fill","black");
+    selectedNode.attr("stroke-width", 3).attr("fill-opacity", 1);
 
     linksToSelectedNode = node.sourceLinks.concat(node.targetLinks);
     for (i = 0; i < linksToSelectedNode.length; i++) {
@@ -123,7 +128,8 @@ dispatch.on("mouseoverNode", function(node){
 });
 dispatch.on("mouseoutNode", function(node){
     console.log("hellomouseout");
-    selectedNode.attr("fill", parallelsets_color(node.name.replace(/ .*/, "")));
+    selectedNode.attr("stroke-width", 1).attr("fill-opacity", 0.7);
+
     //change back link opacity to 0.2
     for (i = 0; i < linksToSelectedNode.length; i++) {
         let htmlLink = d3.select("#link"+ linksToSelectedNode[i].index);
