7611b86553fe1aa00c169e74c143efd4ef899c87
Lenka Obermajerová <Lenka.Obermajerova@seznam.cz>
2019-12-12 18:40:59 +0000

    Barchart tootltip

--- src/js/barchart.js
+++ src/js/barchart.js
@@ -5,22 +5,56 @@ var bchMargin = {top: 20, right: 20, bottom: 40, left: 40};
 bchWidth = 780 - bchMargin.left - bchMargin.right;
 bchHeight = 230 - bchMargin.top - bchMargin.bottom;
 
+let selectedBubbleColor = d3.scaleSequential().domain([0, 0.40]).interpolator(d3.interpolatePuRd);
+
 var bchXscale = d3.scaleBand();
 var bchXaxis = d3.axisBottom();
 var bchYscale = d3.scaleLinear();
 var bchYaxis = d3.axisLeft();
 
+var studentRow;
+var selectedCountryName;
+var bchtotalStudentCount;
+
 var bchBarTip = d3.tip().attr('class', 'd3-tip').html(
-    function(d){
+    function(d, students ){
         var content = "";
-        content += `<span style='margin-left: 1.5px;'><b>` + d.ISO + `</b></span><br>`;
-        content +=`
+        content += `<span style='margin-left: 1.5px;'><b>` + d.name + `</b></span><br>`;
+        if(!selectedCountry || selectedCountry.toLowerCase() === d.ISO.toLowerCase()) {
+            content += `
+                        <table style="margin-top: 2.5px;">
+                                <tr><td style="text-align: left;"> ` + "Cost of living: " + `</td><td>` + d.cost + "\n" + `</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Rental index: " + `</td><td>` + d.RentIndex + "\n" + `</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Beer price: " + `</td><td>` + Math.round(d.DomesticBeer * 10 + Number.EPSILON) / 10 + " \€\n" + `</td> </tr>
+                        </table>
+                        `;
+        } else {
+            var numberStudent;
+            if (selectedCountry) {
+                numberStudent = studentRow.filter(a => a[0].toLowerCase() === d.ISO.toLowerCase())[0];
+                if (studentDirection == "incoming") {
+                    content += `
                         <table style="margin-top: 2.5px;">
                                 <tr><td style="text-align: left;"> ` + "Cost of living: " +`</td><td>`+ d.cost + "\n" +`</td> </tr>
                                 <tr><td style="text-align: left;"> ` + "Rental index: " +`</td><td>`+ d.RentIndex + "\n" +`</td> </tr>
                                 <tr><td style="text-align: left;"> ` + "Beer price: " +`</td><td>`+ Math.round( d.DomesticBeer * 100 + Number.EPSILON ) / 100 + " \€\n" +`</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Students going to " + selectedCountryName + ":" + `</td><td>` + numberStudent[1] + `</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "    → Percentage:" + `</td><td>` + (numberStudent[1]/bchtotalStudentCount * 100).toFixed(1) + "\%" +`</td> </tr>
                         </table>
                         `;
+                } else {
+                    content += `
+                        <table style="margin-top: 2.5px;">
+                                <tr><td style="text-align: left;"> ` + "Cost of living: " +`</td><td>`+ d.cost + "\n" +`</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Rental index: " +`</td><td>`+ d.RentIndex + "\n" +`</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Beer price: " +`</td><td>`+ Math.round( d.DomesticBeer * 100 + Number.EPSILON ) / 100 + " \€\n" +`</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "Students coming from " + selectedCountryName + ":" + `</td><td>` + numberStudent[1] + `</td> </tr>
+                                <tr><td style="text-align: left;"> ` + "    → Percentage:" + `</td><td>` + (numberStudent[1]/bchtotalStudentCount * 100).toFixed(1) + "\%" +`</td> </tr>
+                        </table>
+                        `;
+                }
+            }
+        }
         return content;
     });
 
@@ -34,7 +68,7 @@ var bchSvg = d3.select("#barchart")
         "translate(" + bchMargin.left + "," + bchMargin.top + ")");
 
 
-d3.csv("./data/cost-of-living.csv").then(function (data) {
+d3.csv("./data/costsofliving.csv").then(function (data) {
 bchDataset = data;
 gen_vis();
 });
@@ -89,7 +123,7 @@ function gen_vis() {
         })
         .attr("fill", "#ffca6e")
         .attr("opacity", "0.7")
-        .attr("stroke-width", "0")
+        .attr("stroke-opacity", "0.3")
         .attr("class", "bar")
         .on('mouseover', function(d){
             bchBarTip.show(d);
@@ -111,6 +145,7 @@ function gen_vis() {
         .attr("font-size", 11)
         .attr("y", bchHeight+bchMargin.bottom-10)
         .attr("x", bchWidth/2)
+        .attr("id","xlabel")
         .text("Country");
 
 
@@ -133,6 +168,7 @@ function gen_vis() {
         .attr("y2", bchHeight)
         .attr("stroke", "rgba(0,0,0,0)")
         .attr("id","countryLine");
+
 
     bchSvg.selectAll("indPoints")
         .data(bchDataset)
@@ -168,13 +204,7 @@ function generateRentalPrice() {
             return bchHeight - bchYscale(d.RentIndex);
         })
         .attr("y", function (d) { return bchYscale(d.RentIndex)})
-        .attr("fill", function(d) {
-            if(d.ISO.toLowerCase() === selectedCountry.toLowerCase()){
-                return "red";
-            } else {
-                return "#e6df60";
-            }
-        });
+        .attr("fill", "#e6df60");
 
     if(!selectedCountry) {
         bchSvg.selectAll("rect")
@@ -187,10 +217,6 @@ function generateRentalPrice() {
 
     bchSvg.select("#ylabel")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - (bchMargin.left/2)-8)
-        .attr("x",0 - bchHeight / 2)
-        .attr("fill", "black")
-        .attr("font-size", 11)
         .text("Index");
 }
 
@@ -216,13 +242,7 @@ function generateBeerPrice() {
         .attr("height", function (d) {
             return bchHeight - bchYscale(d.DomesticBeer);
         })
-        .attr("fill", function(d) {
-            if(d.ISO.toLowerCase() === selectedCountry.toLowerCase()){
-                return "red";
-            } else {
-                return "#cda555";
-            }
-        });
+        .attr("fill", "#cda555");
 
     // if(selectedCountry) {
         bchSvg.selectAll("rect")
@@ -235,10 +255,6 @@ function generateBeerPrice() {
 
     bchSvg.select("#ylabel")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - (bchMargin.left / 2) - 8)
-        .attr("x", 0 - bchHeight / 2)
-        .attr("fill", "black")
-        .attr("font-size", 11)
         .text("EUR");
 }
 
@@ -260,13 +276,7 @@ function generateCostOfLiving() {
         .duration(750)
         .attr("height", function(d) { return bchHeight - bchYscale(d.cost); })
         .attr("y", function(d) { return bchYscale(d.cost); })
-        .attr("fill", function(d) {
-            if(d.ISO.toLowerCase() === selectedCountry.toLowerCase()){
-                return "red";
-            } else {
-                return "#ffca6e";
-            }
-        });
+        .attr("fill", "#ffca6e");
 
     if(!selectedCountry) {
         bchSvg.selectAll("rect")
@@ -279,10 +289,6 @@ function generateCostOfLiving() {
 
     bchSvg.select("#ylabel")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - (bchMargin.left/2)-8)
-        .attr("x",0 - bchHeight / 2)
-        .attr("fill", "black")
-        .attr("font-size", 11)
         .text("Index");
 }
 
@@ -308,11 +314,13 @@ function drawBarchart() {
     drawSelectedCountryLine();
     colorBubbles();
 }
-
 function sortBars() {
     sortDataset();
 
     bchXscale.domain(bchDataset.map(function(d) {
+        if(d.ISO.toLowerCase() === selectedCountry.toLowerCase()) {
+            selectedCountryName = d.name;
+        }
         return (d.ISO);
     }));
 
@@ -323,7 +331,6 @@ function sortBars() {
             return bchXscale(d.ISO);
         });
 
-    var studentRow;
     if(selectedCountry) {
         if(studentDirection === "incoming"){
             studentRow = bchStudentData.map(function (d) {
@@ -346,6 +353,14 @@ function sortBars() {
                     return ("outgoing students: " + countryA[1]);
                 });
         }
+        console.log("change x");
+        bchSvg.select("#xlabel")
+            .attr("x", bchWidth/2 - 60)
+            .text("Country (sorted by popularity)");
+    } else {
+        bchSvg.select("#xlabel")
+            .attr("x", bchWidth/2)
+            .text("Country");
     }
 
     var transition = bchSvg.transition().duration(750);
@@ -368,7 +383,7 @@ function sortDataset() {
 }
 
 function sortDatasetOutgoing() {
-    var studentRow = Object.entries(bchStudentData.filter(d => d.country.toLowerCase() === selectedCountry.toLowerCase())[0]);
+    studentRow = Object.entries(bchStudentData.filter(d => d.country.toLowerCase() === selectedCountry.toLowerCase())[0]);
     studentRow.splice(0, 1);
 
     console.log("sorting Outgoing");
@@ -389,7 +404,7 @@ function sortDatasetOutgoing() {
 function sortDatasetIncoming() {
     console.log("sorting Incoming");
 
-     var studentRow = bchStudentData.map(function (d) {
+     studentRow = bchStudentData.map(function (d) {
         return [d["country"], d[selectedCountry]]
     });
 
@@ -423,19 +438,27 @@ function colourSelectedCountry() {
     var dropdownVal = dropdown.value.toLowerCase();
 
     bchSvg.selectAll("rect")
-        .attr("fill", function(d) {
-            if(d.ISO.toLowerCase() === selectedCountry.toLowerCase()){
+        .attr("stroke", function(d) {
+            if (d.ISO.toLowerCase() === selectedCountry.toLowerCase()) {
                 return "red";
             } else {
-                if (dropdownVal === "ri") {
-                    return "#e6df60";
-                } else if (dropdownVal === "bp") {
-                    return "#cda555";
-                } else {
-                    return "#ffca6e";
-                }
+                return "black";
             }
         })
+        .attr("stroke-opacity", function(d) {
+            if (d.ISO.toLowerCase() === selectedCountry.toLowerCase()) {
+                return "1";
+            } else {
+                return "0.3";
+            }
+        })
+        .attr("stroke-width", function(d) {
+        if (d.ISO.toLowerCase() === selectedCountry.toLowerCase()) {
+            return "1.5";
+        } else {
+            return "1";
+        }
+    })
 }
 
 //drawing line for the selected country (and make it invisible again if the country is unselected)
@@ -483,7 +506,6 @@ function drawSelectedCountryLine() {
 function highlightBarchart(highlitedState) {
     bchSvg.selectAll(".bar")
         .attr("opacity", d => (d.ISO.toLowerCase() === highlightedState.toLowerCase()) ? 1 : 0.7)
-        .attr("stroke-width", d => (d.ISO.toLowerCase() === highlightedState.toLowerCase()) ? 1 : 0);
 }
 
 function positionBubbles(){
@@ -538,7 +560,7 @@ function positionBubbles(){
 function colorBubbles() {
     if(selectedCountry)
     {
-        var studentRow;
+        studentRow;
         if(studentDirection === "incoming") {
             studentRow = bchStudentData.map(function (d) {
                 return [d["country"], d[selectedCountry]]
@@ -547,19 +569,32 @@ function colorBubbles() {
             studentRow = Object.entries(bchStudentData.filter(d => d.country.toLowerCase() === selectedCountry.toLowerCase())[0]);
             studentRow.splice(0, 1);
         }
-        var maximum = studentRow.filter(a => a[0].toLowerCase() === bchDataset[0].ISO.toLowerCase())[0];
-        let selectedBubbleColor = d3.scaleSequential().domain([0, maximum[1]]).interpolator(d3.interpolateYlOrBr);
-        console.log("maximum:" + maximum[1]);
+
+        bchtotalStudentCount = studentRow.map(d => d[1]).reduce((x, y) => parseInt(x) + parseInt(y));
 
         bchSvg.selectAll(".studentsBubble")
             // .transition()
             // .duration(750)
-            .attr("stroke", "black")
+            .attr("stroke", function (d) {
+                var tmp = studentRow.filter(function (g) {
+                    return g[0].toLowerCase() === d.ISO.toLowerCase();
+                });
+                if (tmp[0][0].toLowerCase() !== selectedCountry) {
+                    return "black";
+                } else {
+                    return "rgba(0,0,0,0)"
+                }
+            })
+            .attr("stroke-opacity", 0.3)
             .attr("fill", function (d) {
                 var tmp = studentRow.filter(function (g) {
                     return g[0].toLowerCase() === d.ISO.toLowerCase();
                 });
-                return selectedBubbleColor(tmp[0][1]);
+                if (tmp[0][0].toLowerCase() !== selectedCountry) {
+                    return selectedBubbleColor(tmp[0][1] / bchtotalStudentCount);
+                } else {
+                    return "rgba(0,0,0,0)"
+                }
             })
 
     } else {
@@ -571,84 +606,5 @@ function colorBubbles() {
             .attr("fill", "rgba(0,0,0,0)");
     }
 }
-
-/**
- * Draw the legend based on the selected country variable.
- * The legend is removed before creating a new one.
- */
-// function drawLegendBch(legendDomain) {
-//     // Remove the legend if it already exists
-//     if (bchLegendGroup != null) {
-//         bchLegendGroup.remove();
-//     }
-//
-//     if (selectedCountry) {
-//         // Editable options
-//         const bchLegendTicks = 10;
-//         const bchLegendWidth = 240;
-//
-//         const [bchLegendMin, bchLegendMax] = legendDomain;
-//
-//         const [bchLegendPosX, bchLegendPosY] = [bchWidth - bchLegendWidth - 20, 20];
-//         const bchLegendTickWidth = bchLegendWidth / bchLegendTicks;
-//         const bchLegendHeight = bchLegendTickWidth / 2;
-//
-//         let selectedBubbleColor = d3.scaleSequential().domain(bchLegendMin, bchLegendMax).interpolator(d3.interpolateYlOrBr);
-//         bchLegendGroup = bchSVG.append("g")
-//             .attr("class", "legend")
-//             .attr("transform", `translate(${bchLegendPosX}, ${bchLegendPosY})`);
-//
-//         const bch_padding_x = 7;
-//         const bch_padding_y = 5;
-//         bchLegendGroup.append("rect")
-//             .attr("width", bchLegendWidth + bch_padding_x * 2)
-//             .attr("height", (bchLegendHeight + bch_padding_y) * 2)
-//             .attr("transform", `translate(${-bch_padding_x}, ${-(bchLegendHeight + bch_padding_y)})`)
-//             .attr("fill", "white")
-//             .attr("stroke-width", 0.1)
-//             .attr("stroke", "black")
-//             .classed("legend");
-//
-//         bchLegendGroup.selectAll("rect.numbers")
-//             .data(d3.range(bchLegendMin, bchLegendMax, (bchLegendMax - bchLegendMin) / bchLegendTicks))
-//             .enter()
-//             .append("rect")
-//             .attr("width", bchLegendTickWidth)
-//             .attr("height", bchLegendHeight)
-//             .attr("stroke", "black")
-//             .attr("stroke-width", 0.01)
-//             .attr("x", function (d, i) {
-//                 return i * bchLegendTickWidth;
-//             })
-//             .attr("fill", d=>selectedBubbleColor(d))
-//             .classed("numbers");
-//
-//         bchLegendGroup.selectAll("text")
-//             .data(d3.range(bchLegendMin, bchLegendMax, (bchLegendMax - bchLegendMin) / bchLegendTicks))
-//             .enter()
-//             .append("text")
-//             .attr("font-size", 7)
-//             .attr("fill", "black")
-//             .attr("y", bchLegendHeight - 3)
-//             .attr("x", function (d, i) {
-//                 return i * bchLegendTickWidth + 3;
-//             }).text((d, i) => {
-//             // Only draw every third tick
-//             if (i % 3 === 0) {
-//                 return selectedCountry === ""
-//                     ? d.toFixed(1)
-//                     : (d * 100).toFixed(1) + "%";
-//             }
-//         });
-//
-//         bchLegendGroup
-//             .append("text")
-//             .attr("fill", "black")
-//             .attr("font-size", 11)
-//             .attr("y", -3)
-//             .attr("x", 4)
-//             .text("number of students" + studentDirection);
-//     }
-// }
 //barevná škála pro znázormění počtu studentů
 //z pevné šířky na procenta - zmízí osy
